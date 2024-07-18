729fca80b43f5f373208c523b644fa45f0e35939
KimDoHyun <kdhqwe1030@naver.com>
2024-07-18 23:08:26 +0900

    feat: Enterprise Apply Page and fix Login Button opacity

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
-
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-
-import React, { useEffect, useState, createContext } from "react";
+import React, { useEffect, useState, createContext } from 'react';
 
 import { NavermapsProvider } from 'react-naver-maps';
 
@@ -17,7 +15,7 @@ import Companydetails from './pages/CompanyDetails';
 import JobNoticeDetails from './pages/JobNoticeDetails';
 import SingIn from './pages/SingIn';
 import EnterpriseApply from './pages/EnterpriseApply';
-  export const JobContext = createContext();
+export const JobContext = createContext();
 export const CompanyContext = createContext();
 export const LoginContext = createContext();
 export const ReviewContext = createContext();
@@ -25,87 +23,76 @@ function App() {
   const [jobs, setJobs] = useState([]);
   const [companies, setCompanies] = useState([]);
   const [reviews, setReviews] = useState([]);
-const [loginState, setLoginState] = useState({
+  const [loginState, setLoginState] = useState({
     userLogin: false,
     userEnterprise: false,
   });
   useEffect(() => {
-    fetch("http://localhost:8080/api/jobs")
+    fetch('http://localhost:8080/api/jobs')
       .then((response) => response.json())
       .then((data) => setJobs(data))
-      .catch((error) => console.error("Error fetching jobs:", error));
+      .catch((error) => console.error('Error fetching jobs:', error));
   }, []);
 
   useEffect(() => {
-    fetch("http://localhost:8080/api/enterprises")
+    fetch('http://localhost:8080/api/enterprises')
       .then((response) => response.json())
       .then((data) => setCompanies(data))
-      .catch((error) => console.error("Error fetching companies:", error));
+      .catch((error) => console.error('Error fetching companies:', error));
   }, []);
 
   useEffect(() => {
-    fetch("http://localhost:8080/api/reviews")
+    fetch('http://localhost:8080/api/reviews')
       .then((response) => response.json())
       .then((data) => setReviews(data))
-      .catch((error) => console.error("Error fetching reviews:", error));
+      .catch((error) => console.error('Error fetching reviews:', error));
   }, []);
 
-  
-  
-  
-  
-  
-  
-  
-  
   return (
     <NavermapsProvider ncpClientId="du60d8o1se">
-     <LoginContext.Provider value={{ loginState, setLoginState }}>
-      <JobContext.Provider value={jobs}>
-        <CompanyContext.Provider value={companies}>
-          <ReviewContext.Provider value={reviews}>
+      <LoginContext.Provider value={{ loginState, setLoginState }}>
+        <JobContext.Provider value={jobs}>
+          <CompanyContext.Provider value={companies}>
+            <ReviewContext.Provider value={reviews}>
+              <BrowserRouter>
+                <Routes>
+                  <Route path="/" element={<Home />} />
+                  <Route path="/CompanyInfo" element={<CompanyInfo />} />
+                  <Route path="/JobNotice" element={<JobNotice />} />
+                  <Route path="/IegalAdvice" element={<IegalAdvice />} />
+                  <Route path="/Support" element={<Support />} />
+                  <Route path="/SingIn" element={<SingIn />} />
 
+                  <Route path="/Search/:keyword" element={<Search />} />
+                  <Route path="/AiSearch/:keyword" element={<Search />} />
+                  <Route
+                    path="/Search/:keyword/:enterprise_id"
+                    element={<Companydetails />}
+                  />
+                  <Route
+                    path="/CompanyInfo/:enterprise_id"
+                    element={<Companydetails />}
+                  />
+                  <Route
+                    path="/Search/:keyword/jobNotice/:job_id"
+                    element={<JobNoticeDetails />}
+                  />
+                  <Route
+                    path="/JobNotice/:job_id"
+                    element={<JobNoticeDetails />}
+                  />
 
-
-
-
-            <BrowserRouter>
-              <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/CompanyInfo" element={<CompanyInfo />} />
-                <Route path="/JobNotice" element={<JobNotice />} />
-                <Route path="/IegalAdvice" element={<IegalAdvice />} />
-                <Route path="/Support" element={<Support />} />
-                <Route path="/SingIn" element={<SingIn />} />
-
-                <Route path="/Search/:keyword" element={<Search />} />
-                <Route path="/AiSearch/:keyword" element={<Search />} />
-                <Route
-                  path="/Search/:keyword/:enterprise_id"
-                  element={<Companydetails />}
-                />
-                <Route
-                  path="/CompanyInfo/:enterprise_id"
-                  element={<Companydetails />}
-                />
-                <Route
-                  path="/Search/:keyword/jobNotice/:job_id"
-                  element={<JobNoticeDetails />}
-                />
-                <Route
-                  path="/JobNotice/:job_id"
-                  element={<JobNoticeDetails />}
-                />
-
-                <Route path="/EnterpriseApply" element={<EnterpriseApply />} />
-                <Route path="*" element={<Notfound />} />
-              </Routes>
-            </BrowserRouter>
-          </ReviewContext.Provider>
+                  <Route
+                    path="/EnterpriseApply"
+                    element={<EnterpriseApply />}
+                  />
+                  <Route path="*" element={<Notfound />} />
+                </Routes>
+              </BrowserRouter>
+            </ReviewContext.Provider>
           </CompanyContext.Provider>
         </JobContext.Provider>
       </LoginContext.Provider>
-
     </NavermapsProvider>
   );
 }
