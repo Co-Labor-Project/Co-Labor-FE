82f9dd57f71296e4a5c7426034d3abb5e1e350ad
JoJunHwa <jjj5306@naver.com>
2024-07-19 22:52:10 +0900

    feat : add comapny style

--- src/components/CompanyDetailsCenter.jsx
+++ src/components/CompanyDetailsCenter.jsx
@@ -49,41 +49,52 @@ const CompanyDetailsCenter = () => {
   const displayJobPhoto = (job) => job.photo || defaultJobPhoto;
 
   return (
-    <div>
+    <div className="CompanyDetail">
       <br />
       <br />
       <div className="CompanyDetailsCenterCompany">
         <h1 className="title">{companyData.name} 기본 정보</h1>
         <div className="gap"></div>
         <div className="CompanyDetailsCenterCompanyBasicInfo">
-          <div>
+          <div className="CompanyDetailsCenterCompanyImgContainer">
             <img
+              className="CompanyDetailsCenterCompanyImg"
               src={displayCompanyPhoto}
-              width="100px"
               alt={companyData.name}
             />
           </div>
-          <div>
-            주소: {companyData.address1} {companyData.address2}{" "}
-            {companyData.address3}
+          <div className="CompanyDetailsCenterCompanyTextContainer">
+            <div className="CompanyDetailsCenterCompanyText">
+              <div className="CompanyDetailsCenterCompanyAddress">
+                주소: {companyData.address1} {companyData.address2}{" "}
+                {companyData.address3}
+              </div>
+              <div className="CompanyDetailsCenterCompanyPh">
+                전화번호: {companyData.phone_number}
+              </div>
+              <div className="CompanyDetailsCenterCompanyType">
+                기업 분류: {displayCompanyType}
+              </div>
+              <div className="CompanyDetailsCenterCompanyDes">
+                기업 설명: {displayCompanyDescription}
+              </div>
+            </div>
           </div>
-          <div>전화번호: {companyData.phone_number}</div>
-          <div>기업 분류: {displayCompanyType}</div>
-          <div>기업 설명: {displayCompanyDescription}</div>
         </div>
       </div>
+
       <div className="gap"></div>
-      <div className="gap"></div>
+      <div className="gap2"></div>
 
       <div className="CompanyDetailsCenterJob">
         <h1 className="title">{companyData.name} 채용 공고</h1>
-        <div className="gap"></div>
+        <div className="gap2"></div>
         <div className="CompanyDetailsCenterJobList">
           {jobData.length > 0 ? (
             <Swiper
               modules={[Navigation, Pagination]}
-              spaceBetween={-150}
-              slidesPerView={4}
+              spaceBetween={-110}
+              slidesPerView={3}
               navigation
               pagination={{ clickable: true }}
             >
@@ -122,7 +133,7 @@ const CompanyDetailsCenter = () => {
       </div>
 
       <div className="gap"></div>
-      <div className="gap"></div>
+      <div className="gap2"></div>
 
       <div className="CompanyDetailsCenterReview">
         <h1 className="title">{companyData.name} 리뷰</h1>
