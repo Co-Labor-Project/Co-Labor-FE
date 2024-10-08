5538e1c2e69ef60af2eaba7fb0e424d601c44fac
KimDoHyun <kdhqwe1030@naver.com>
2024-10-08 19:57:40 +0900

    fix: Not Found 페이지 style

--- src/pages/Notfound.jsx
+++ src/pages/Notfound.jsx
@@ -2,7 +2,8 @@ import styled from 'styled-components';
 const Notfound = () => {
   return (
     <BackGround>
-      <h1>잘못된 페이지 접근 입니다.</h1>
+      <Text404>4 0 4</Text404>
+      <Text>Page Notfound</Text>
     </BackGround>
   );
 };
@@ -13,4 +14,21 @@ const BackGround = styled.div`
   height: 95vh;
   align-items: center;
   justify-content: center;
+  flex-direction: column;
+  gap: 30px;
+`;
+
+const Text404 = styled.div`
+  font-size: 160px;
+  font-weight: 800;
+  background: linear-gradient(to right top, #861657, #f48378);
+  color: transparent;
+  -webkit-background-clip: text;
+`;
+const Text = styled.div`
+  font-size: 50px;
+  font-weight: 800;
+  background: linear-gradient(to right top, #861657, #f48378);
+  color: transparent;
+  -webkit-background-clip: text;
 `;
