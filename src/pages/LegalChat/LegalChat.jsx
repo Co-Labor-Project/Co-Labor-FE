098da1de216cfba80015753ce1a14e0ee54def08
KimDoHyun <kdhqwe1030@naver.com>
2024-10-11 16:02:31 +0900

    feat: 초기화면 세팅 구축

--- src/pages/LegalChat/LegalChat.jsx
+++ src/pages/LegalChat/LegalChat.jsx
@@ -122,7 +122,6 @@ const BaseContainer = styled.div`
   width: 1000px;
   height: 100%;
   background: #fff;
-  /* box-shadow: 0px 14px 24px rgba(0, 0, 0, 0.1); */
   overflow: hidden;
   display: flex;
   flex-direction: column;
